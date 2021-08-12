dc31ca4121bc853ebfbe835495b34aba930084f9
Kirill Petukhov <kir2486@gmail.com>
2021-08-12 15:47:47 +0300

    initial commit

---
+++ Проекты/1/JS/script.js
@@ -0,0 +1,18 @@
+console.log(1);
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+
+const obj = {
+    a:50
+};
+
+obj.a = 450;
+
+console.log(obj);
+
+
+var name = 'Ivan';
